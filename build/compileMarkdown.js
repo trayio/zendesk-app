81b00c5c078d026d12a5969a60d095744d3ce0ed
Alex Burley <alex.burley@tray.io>
2020-03-16 11:16:47 +0000

    Fix build

--- build/compileMarkdown.js
+++ build/compileMarkdown.js
@@ -1,42 +1,38 @@
 /*
-* A simple script to take the `long_description` and put it into the 
-* `translations/en.json` file.
-*/
-const fs = require('fs');
+ * A simple script to take the `long_description` and put it into the
+ * `translations/en.json` file.
+ */
+const fs = require("fs");
 
+const keysToCompile = ["long_description", "installation_instructions"];
+const apps = ["support", "sell"];
 
-
-const keysToCompile = ['long_description', 'installation_instructions'];
-
-keysToCompile.forEach((key) => {
-
-
+apps.forEach(appKey => {
+  keysToCompile.forEach(key => {
     console.log(`Compiling ${key}.md...`);
-
 
     // Read the existing translation
     let translation;
     try {
-        const json = fs.readFileSync(`${__dirname}/../app/translations/en.json`).toString();
-        translation = JSON.parse(json);
+      const json = fs
+        .readFileSync(`${__dirname}/../app/translations/en.json`)
+        .toString();
+      translation = JSON.parse(json);
     } catch (e) {
-        throw e;
+      throw e;
     }
 
-
     // Set the new description based on the markdown
-    const description = fs.readFileSync(`${__dirname}/../app/copy/${key}.md`).toString();
-    translation.app[key] = description;
-
+    const description = fs
+      .readFileSync(`${__dirname}/../app/copy/${key}.md`)
+      .toString();
+    translation.app[appKey][key] = description;
 
     // Update the new JSON pfile
-    const prettyJson = JSON.stringify(translation, null, '  ');
+    const prettyJson = JSON.stringify(translation, null, "  ");
     fs.writeFileSync(`${__dirname}/../app/translations/en.json`, prettyJson);
 
-
     console.log(`Compiled ${key}.md: SUCCESS`);
-    console.log('');
-
+    console.log("");
+  });
 });
-
-
